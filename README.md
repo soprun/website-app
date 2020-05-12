356413aa11ab1ac76783db9acfccaba9ff3f174b
Vladislav Soprun <develop@soprun.com>
2020-05-12 07:31:33 +0300

    Configure Google Cloud Platform

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Big Data Technology
+# React.js single page application.
+- website: soprun.com
 
-app
+# ghost.io: publishing platform
+RESTful JSON API
+- Core API: Admin - Does not change.
+- Core API: Content - Uses React.js single page application.
+
+# Google Cloud Platform: configure default
+- project_id: `project-40825`
+- project_region: `europe-west3`
+- project_zone: `europe-west3-a`
+- *parameter: value_to_replace*
+
+## Google cloud platform: Kubernetes apps
+- [ ] Ingress: nginx
+- [ ] TSL/SSL certificate: cert-manager
+
+
+## Cloud Build for continuous integration
+
+## Architecture
--- gcloud/cluster-create.sh
+++ gcloud/cluster-create.sh
@@ -5,9 +5,9 @@ source ./env.sh
 # printenv | sort
 # exit;
 
-GKE_CLUSTER_NAME="sandbox_cluster"
-
 # Configure: Google Kubernetes Engine (GKE)
+
+GKE_CLUSTER_NAME="sandbox_cluster"
 
 number_nodes=3 # default=3 The number of nodes to be created in each of the cluster's zones.
 min_nodes_size=0
@@ -15,17 +15,6 @@ max_nodes_size=3
 
 max_surge_upgrade=1
 max_unavailable_upgrade=0
-
-# --cluster-ipv4-cidr=10.0.0.0/14
-
-# --max-nodes-per-pool=MAX_NODES_PER_POOL
-# --default-max-nodes-per-pool=MAX_NODES_PER_POOL
-# The maximum number of nodes to allocate per default initial node pool.
-
-# --max-pods-per-node=MAX_PODS_PER_NODE
-# --default-max-pods-per-node=MAX_PODS_PER_NODE
-# The max number of pods per node for this node pool.
-
 
 gcloud container clusters create ${GKE_CLUSTER_NAME} \
   --zone=${GKE_CLUSTER_LOCATION} \
@@ -46,5 +35,4 @@ gcloud container clusters create ${GKE_CLUSTER_NAME} \
   --shielded-integrity-monitoring \
   --shielded-secure-boot \
   --resource-usage-bigquery-dataset "cluster_usage_metering" \
-  --enable-resource-consumption-metering \
-
+  --enable-resource-consumption-metering
