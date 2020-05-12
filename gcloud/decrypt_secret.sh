0f4931861f7fe14e31d1763b10f00f60d0631fe8
Vladislav Soprun <develop@soprun.com>
2020-05-12 20:00:48 +0300

    signed commit message...

--- gcloud/decrypt_secret.sh
+++ gcloud/decrypt_secret.sh
@@ -13,7 +13,8 @@ export GPG_SECRET_PASSPHRASE=${GPG_SECRET_PASSPHRASE:-secret}
 # Decrypt the file
 #mkdir $HOME/secrets
 
-ls -la "$HOME/gcloud"
+printenv | sort
+
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg \
@@ -22,4 +23,4 @@ gpg \
   --yes \
   --decrypt \
   --passphrase=${GPG_SECRET_PASSPHRASE} \
-  --output secret/decrypt.json secret/encrypt.json.gpg
+  --output "$PWD/gcloud/secret/decrypt.json" "$PWD/gcloud/secret/encrypt.json.gpg"
