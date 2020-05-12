0058a7e56aa18d2e89be37d46bf46a024021b79b
Vladislav Soprun <develop@soprun.com>
2020-05-12 19:49:35 +0300

    Update nodejs.yml

---
+++ gcloud/decrypt_secret.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export GPG_SECRET_PASSPHRASE=${GPG_SECRET_PASSPHRASE:-secret}
+
+#gpg \
+#  --batch \
+#  --cipher-algo AES256 \
+#  --passphrase="${passphrase}" \
+#  --output ./secret/encrypt.json.gpg \
+#  --symmetric ./secret.json \
+
+#exit;
+# Decrypt the file
+#mkdir $HOME/secrets
+
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg \
+  --quiet \
+  --batch \
+  --yes \
+  --decrypt \
+  --passphrase=${GPG_SECRET_PASSPHRASE} \
+  --output ./secret/decrypt.json ./secret/encrypt.json.gpg
